0d08f5184cdc6c64361edf1324f4181384a5c7b9
Alexandra Gumalal <gumalal.alexandra@iisgalvanimi.edu.it>
2026-01-22 09:00:02 +0000

    Es completato

--- gumalal/src/app/app.ts
+++ gumalal/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// ... altre importazioni in alto ...
+import { Component } from '@angular/core';
+import { SecondComponent } from './second-component/second-component';
+import { ThirdComponent } from './third-component/third-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SecondComponent, ThirdComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('gumalal');
+export class AppComponent {
+  name = 'app';
 }
--- gumalal/src/app/second-component/second-component.ts
+++ gumalal/src/app/second-component/second-component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './second-component.css',
 })
 export class SecondComponent {
-
+name = 'secondo';
 }
